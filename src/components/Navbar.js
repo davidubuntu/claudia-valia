6bd78cc0fe4112c741856b6089fde1dbc1118c68
David Diez <d.diezr@gmail.com>
2020-04-05 22:30:24 +0200

    feat: navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import './Navbar.sass'
+const Navbar = (props)=> {
+const [active, setActive] = useState(false)
+const [navBarActiveClass,SetNavBarActiveClass] = useState('')
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
+useEffect(() => {
+	// set the class in state for the navbar accordingly
+	active ?  SetNavBarActiveClass('is-active') :  SetNavBarActiveClass('')
+}, [active])
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
+const toggleHamburger = () => {
+	// toggle the active boolean in the state
+	setActive(!active)
+}
 
-  render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="nav-main"
         role="navigation"
         aria-label="main-navigation"
       >
-        <div className="container">
+        <div className="nav-container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              {/* <img src={logo} alt="CVALIA" style={{ width: '88px' }} /> */}
+							<span>C VALIA</span>
             </Link>
             {/* Hamburger menu */}
             <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+              className={`navbar-burger burger ${navBarActiveClass}`}
               data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
+              onClick={() => toggleHamburger()}
             >
               <span />
               <span />
@@ -57,7 +42,7 @@ const Navbar = class extends React.Component {
           </div>
           <div
             id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-menu ${navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
@@ -76,23 +61,10 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
           </div>
         </div>
       </nav>
     )
-  }
 }
 
 export default Navbar
